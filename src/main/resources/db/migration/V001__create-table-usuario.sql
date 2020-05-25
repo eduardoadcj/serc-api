54207f22996b0d27a17e349111c19fad209541bd
Eduardo A. Cruz Junior <eduardo_acj2@hotmail.com>
2020-05-25 19:14:58 -0300

    condensando criacao do banco de dados em apenas um arquivo

--- src/main/resources/db/migration/V001__create-table-usuario.sql
+++
@@ -1,5 +0,0 @@
-create table usuario (
-    id bigint not null auto_increment primary key,
-    uid varchar(20) not null,
-    nome varchar(200) not null
-);
--- src/main/resources/db/migration/V001__create.sql
+++ src/main/resources/db/migration/V001__create.sql
@@ -1,3 +1,25 @@
+create table usuario (
+    id bigint not null auto_increment primary key,
+    uid varchar(20) not null,
+    nome varchar(200) not null
+);
+
+create table cliente (
+    id bigint not null auto_increment primary key,
+    nome varchar(200) not null,
+    whatsapp varchar(11),
+    email varchar(255),
+    cpf char(11) not null,
+    numero_calcado varchar(3),
+    numero_jeans varchar(3),
+    nascimento datetime,
+    data_registro datetime not null,
+    id_usuario bigint not null
+);
+
+alter table cliente add constraint fk_cliente_usuario
+foreign key (id_usuario) references usuario(id);
+
 create table estado (
     id bigint not null auto_increment primary key,
     nome varchar(200) not null,
