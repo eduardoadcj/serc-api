853157c4ba9a703a6edac8d63a27a19fef594c1f
Eduardo A. Cruz Junior <eduardo_acj2@hotmail.com>
2020-05-25 16:21:03 -0300

    first up

---
+++ src/main/java/com/eacj/sercapi/SercApiApplication.java
@@ -0,0 +1,13 @@
+package com.eacj.sercapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SercApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SercApiApplication.class, args);
+    }
+
+}
---
+++ src/main/resources/db/migration/V001__create-endereco.sql
@@ -0,0 +1,27 @@
+create table estado (
+    id bigint not null auto_increment primary key,
+    nome varchar(200) not null,
+    uf char(2) not null
+);
+
+create table cidade (
+    id bigint not null auto_increment primary key,
+    nome varchar(200) not null,
+    id_estado bigint not null
+);
+
+alter table cidade add constraint fk_cidade_estado
+foreign key (id_estado) references estado (id);
+
+create table endereco (
+    id bigint not null auto_increment primary key,
+    titulo varchar(200) not null,
+    rua varchar(255) not null,
+    bairro varchar(255) not null,
+    numero varchar(20) not null,
+    cep char(8) not null,
+    id_cidade bigint not null
+);
+
+alter table endereco add constraint fk_endereco_cidade
+foreign key (id_cidade) references cidade (id);
