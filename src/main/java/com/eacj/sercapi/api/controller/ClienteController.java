2a6d6bc7886a8c42ee8ed028a5837e694e5851ce
Eduardo A. Cruz Junior <eduardo_acj2@hotmail.com>
2020-06-30 18:54:25 -0300

    implementacao do endpoint consulta

--- src/main/java/com/eacj/sercapi/api/controller/ClienteController.java
+++ src/main/java/com/eacj/sercapi/api/controller/ClienteController.java
@@ -7,6 +7,8 @@ import com.eacj.sercapi.domain.model.Cliente;
 import com.eacj.sercapi.domain.model.Usuario;
 import com.eacj.sercapi.domain.repository.ClienteRepository;
 import com.eacj.sercapi.domain.service.CadastroClienteService;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -14,6 +16,7 @@ import javax.validation.Valid;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -37,7 +40,6 @@ public class ClienteController {
 
     private final static int DEFAULT_PAGE_SIZE = 25;
 
-    
     @Autowired
     private ClienteRepository clienteRepository;
 
@@ -46,28 +48,28 @@ public class ClienteController {
 
     @Autowired
     private ModelMapper modelMapper;
-    
+
     @Autowired
     private AuthenticatedUserService authenticatedUserService;
 
-    
     @GetMapping("/all")
-    public List<ClienteModel> listar(){
+    public List<ClienteModel> listar() {
         List<Cliente> list = clienteRepository.findByUsuarioId(
                 authenticatedUserService.getAuthenticatedUsuario().getId());
         return toCollectionModel(list);
     }
-    
+
     @GetMapping
     public ResponseEntity<List<ClienteModel>> listar(@RequestParam int page) {
 
         HttpHeaders responseHeaders = new HttpHeaders();
-        
-        if (page < 0) 
+
+        if (page < 0) {
             return ResponseEntity.badRequest().build();
-        
+        }
+
         Usuario usuario = authenticatedUserService.getAuthenticatedUsuario();
-        
+
         Pageable sortByName = PageRequest.of(page, DEFAULT_PAGE_SIZE, Sort.by("nome"));
         Page<Cliente> pages = clienteRepository.findByUsuarioId(usuario.getId(), sortByName);
 
@@ -78,75 +80,172 @@ public class ClienteController {
                 .body(toCollectionModel(pages.toList()));
 
     }
-    
+
     @GetMapping("/nome")
-    public List<ClienteModel> listarNome(@RequestParam String nome){
-        
-        List<Cliente> list = clienteRepository
-                .findByUsuarioIdAndNomeContainingIgnoreCaseOrderByNomeAsc(
+    public ResponseEntity<List<ClienteModel>> listarNome(@RequestParam int page, @RequestParam String nome) {
+
+        HttpHeaders responseHeaders = new HttpHeaders();
+
+        if (page < 0) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        Pageable sortByName = PageRequest.of(page, DEFAULT_PAGE_SIZE, Sort.by("nome"));
+        Page<Cliente> pages = clienteRepository
+                .findByUsuarioIdAndNomeContainingIgnoreCase(
                         authenticatedUserService.getAuthenticatedUsuario().getId(),
-                        nome);
-        
-        return toCollectionModel(list);
-        
+                        nome,
+                        sortByName);
+
+        responseHeaders.add("X-Total-Count", String.valueOf(pages.getTotalElements()));
+
+        return ResponseEntity.ok()
+                .headers(responseHeaders)
+                .body(toCollectionModel(pages.toList()));
+
     }
-    
+
+    @GetMapping("/consulta")
+    public ResponseEntity<List<ClienteModel>> consultar(@RequestParam int page,
+            @RequestParam String nome,
+            @RequestParam String numeroCalcado,
+            @RequestParam String numeroJeans) {
+
+        HttpHeaders responseHeaders = new HttpHeaders();
+
+        if ((nome.isEmpty() && numeroCalcado.isEmpty() && numeroJeans.isEmpty()) || page < 0) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        Pageable sortByName = PageRequest.of(page, DEFAULT_PAGE_SIZE, Sort.by("nome"));
+        Page<Cliente> pages = null;
+
+        if (!nome.isEmpty()) {
+
+            if (!numeroCalcado.isEmpty() && !numeroJeans.isEmpty()) {
+                pages = clienteRepository
+                        .findByUsuarioIdAndNomeContainingIgnoreCaseAndNumeroCalcadoAndNumeroJeans(
+                                authenticatedUserService.getAuthenticatedUsuario().getId(),
+                                nome,
+                                numeroCalcado,
+                                numeroJeans,
+                                sortByName);
+            } else if (!numeroCalcado.isEmpty()) {
+                pages = clienteRepository
+                        .findByUsuarioIdAndNomeContainingIgnoreCaseAndNumeroCalcado(
+                                authenticatedUserService.getAuthenticatedUsuario().getId(),
+                                nome,
+                                numeroCalcado,
+                                sortByName);
+            } else if (!numeroJeans.isEmpty()) {
+                pages = clienteRepository
+                        .findByUsuarioIdAndNomeContainingIgnoreCaseAndNumeroJeans(
+                                authenticatedUserService.getAuthenticatedUsuario().getId(),
+                                nome,
+                                numeroJeans,
+                                sortByName);
+            } else {
+                pages = clienteRepository
+                        .findByUsuarioIdAndNomeContainingIgnoreCase(
+                                authenticatedUserService.getAuthenticatedUsuario().getId(),
+                                nome,
+                                sortByName);
+            }
+
+        } else {
+
+            if (!numeroCalcado.isEmpty() && !numeroJeans.isEmpty()) {
+                pages = clienteRepository
+                        .findByUsuarioIdAndNumeroCalcadoAndNumeroJeans(
+                                authenticatedUserService.getAuthenticatedUsuario().getId(),
+                                numeroCalcado,
+                                numeroJeans,
+                                sortByName);
+            } else if (!numeroCalcado.isEmpty()) {
+                pages = clienteRepository
+                        .findByUsuarioIdAndNumeroCalcado(
+                                authenticatedUserService.getAuthenticatedUsuario().getId(),
+                                numeroCalcado,
+                                sortByName);
+            } else if (!numeroJeans.isEmpty()) {
+                pages = clienteRepository
+                        .findByUsuarioIdAndNumeroJeans(
+                                authenticatedUserService.getAuthenticatedUsuario().getId(),
+                                numeroJeans,
+                                sortByName);
+            }
+
+        }
+        
+        if(pages == null){
+            return ResponseEntity.notFound().build();
+        }
+        
+        responseHeaders.add("X-Total-Count", String.valueOf(pages.getTotalElements()));
+        
+        return ResponseEntity.ok()
+                .headers(responseHeaders)
+                .body(toCollectionModel(pages.toList()));
+
+    }
+
     @GetMapping("/numeroCalcado")
-    public List<ClienteModel> listarNumeroCalcado(@RequestParam String numeroCalcado){
-        
+    public List<ClienteModel> listarNumeroCalcado(@RequestParam String numeroCalcado) {
+
         List<Cliente> list = clienteRepository.findByUsuarioIdAndNumeroCalcadoOrderByNomeAsc(
-                        authenticatedUserService.getAuthenticatedUsuario().getId(), numeroCalcado);
-        
+                authenticatedUserService.getAuthenticatedUsuario().getId(), numeroCalcado);
+
         return toCollectionModel(list);
-        
+
     }
-    
+
     @GetMapping("/numeroJeans")
-    public List<ClienteModel> listarNumeroJeans(@RequestParam String numeroJeans){
-        
+    public List<ClienteModel> listarNumeroJeans(@RequestParam String numeroJeans) {
+
         List<Cliente> list = clienteRepository.findByUsuarioIdAndNumeroJeansOrderByNomeAsc(
-                        authenticatedUserService.getAuthenticatedUsuario().getId(), numeroJeans);
-        
+                authenticatedUserService.getAuthenticatedUsuario().getId(), numeroJeans);
+
         return toCollectionModel(list);
-        
+
     }
-    
+
     @GetMapping("/numeroCalcado/numeroJeans")
     public List<ClienteModel> listarNumeroCalcadoNumeroJeans(
             @RequestParam String numeroCalcado,
-            @RequestParam String numeroJeans){
-        
+            @RequestParam String numeroJeans) {
+
         List<Cliente> list = clienteRepository.findByUsuarioIdAndNumeroCalcadoAndNumeroJeansOrderByNomeAsc(
-                        authenticatedUserService.getAuthenticatedUsuario().getId(), numeroCalcado, numeroJeans);
-        
+                authenticatedUserService.getAuthenticatedUsuario().getId(), numeroCalcado, numeroJeans);
+
         return toCollectionModel(list);
-        
+
     }
-    
+
     @GetMapping("/numeroJeans/numeroCalcado")
     public List<ClienteModel> listarNumeroJeansNumeroCalcado(
             @RequestParam String numeroCalcado,
-            @RequestParam String numeroJeans){
-        
+            @RequestParam String numeroJeans) {
+
         List<Cliente> list = clienteRepository.findByUsuarioIdAndNumeroCalcadoAndNumeroJeansOrderByNomeAsc(
-                        authenticatedUserService.getAuthenticatedUsuario().getId(), numeroCalcado, numeroJeans);
-        
+                authenticatedUserService.getAuthenticatedUsuario().getId(), numeroCalcado, numeroJeans);
+
         return toCollectionModel(list);
-        
+
     }
-    
+
     @GetMapping("/id/{id}")
     public ResponseEntity<ClienteModel> buscarPorId(@PathVariable Long id) {
-        
+
         Usuario usuario = authenticatedUserService.getAuthenticatedUsuario();
         Optional<Cliente> opCliente = clienteRepository.findByIdAndUsuarioId(id,
                 usuario.getId());
-        
-        if (!opCliente.isPresent()) 
+
+        if (!opCliente.isPresent()) {
             return ResponseEntity.notFound().build();
-        
+        }
+
         return ResponseEntity.ok(toModel(opCliente.get()));
-        
+
     }
 
     @PostMapping
@@ -176,18 +275,17 @@ public class ClienteController {
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> remover(@PathVariable Long id) {
-        
-        if (!clienteRepository.existsById(id)) 
+
+        if (!clienteRepository.existsById(id)) {
             return ResponseEntity.notFound().build();
-        
+        }
+
         cadastroClienteService.remover(id);
         return ResponseEntity.noContent().build();
 
     }
 
-    
     //Entity parsers
-    
     private ClienteModel toModel(Cliente cliente) {
         return modelMapper.map(cliente, ClienteModel.class);
     }
--- src/main/java/com/eacj/sercapi/domain/repository/ClienteRepository.java
+++ src/main/java/com/eacj/sercapi/domain/repository/ClienteRepository.java
@@ -16,9 +16,31 @@ public interface ClienteRepository extends JpaRepository<Cliente, Long>{
     Optional<Cliente> findByCpf(String cpf);
     Optional<Cliente> findByUsuarioIdAndCpf(Long id, String cpf);
     List<Cliente> findByUsuarioId(Long id);
+    
     Page<Cliente> findByUsuarioId(Long id, Pageable pageable);
-
-    List<Cliente> findByUsuarioIdAndNomeContainingIgnoreCaseOrderByNomeAsc(Long id, String nome);
+    
+    
+    Page<Cliente> findByUsuarioIdAndNomeContainingIgnoreCase(Long id, String nome, Pageable pageable);
+    
+    Page<Cliente> findByUsuarioIdAndNomeContainingIgnoreCaseAndNumeroCalcadoAndNumeroJeans(Long id,
+            String nome, String numeroCalcado, String numeroJeans, Pageable pageable);
+    
+    Page<Cliente> findByUsuarioIdAndNomeContainingIgnoreCaseAndNumeroCalcado(Long id,
+            String nome, String numeroCalcado, Pageable pageable);
+    
+    Page<Cliente> findByUsuarioIdAndNomeContainingIgnoreCaseAndNumeroJeans(Long id,
+            String nome, String numeroJeans, Pageable pageable);
+    
+   
+    Page<Cliente> findByUsuarioIdAndNumeroCalcadoAndNumeroJeans(Long id,
+            String numeroCalcado, String numeroJeans, Pageable pageable);
+    
+    Page<Cliente> findByUsuarioIdAndNumeroCalcado(Long id,
+            String numeroJeans, Pageable pageable);
+    
+    Page<Cliente> findByUsuarioIdAndNumeroJeans(Long id,
+            String numeroJeans, Pageable pageable);
+    
     List<Cliente> findByUsuarioIdAndNumeroCalcadoOrderByNomeAsc(Long id, String numeroCalcado);
     List<Cliente> findByUsuarioIdAndNumeroJeansOrderByNomeAsc(Long id, String numeroJeans);
     List<Cliente> findByUsuarioIdAndNumeroCalcadoAndNumeroJeansOrderByNomeAsc(Long id, String numeroCalcado, String numeroJeans);
